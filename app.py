ac842a456cb5896332230d6c6cddb4a8e6367f61
cgai <gaillard@brocelia.fr>
2022-10-24 19:49:54 +0200

    ajout init function

--- app.py
+++ app.py
@@ -85,11 +85,12 @@ async def messages(req: Request) -> Response:
         return json_response(data=response.body, status=response.status)
     return Response(status=HTTPStatus.OK)
 
-
-APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
+def init_function(argv):
+    APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
 
 if __name__ == "__main__":
+    APP = init_function(None)
     try:
         web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:

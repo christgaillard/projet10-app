021281f39d213f9b3b29d1a4d6b6f1509f899423
cgai <gaillard@brocelia.fr>
2022-10-31 16:52:15 +0100

    changement sur les dates

--- helpers/luis_helper.py
+++ helpers/luis_helper.py
@@ -4,9 +4,8 @@ from enum import Enum
 from typing import Dict
 from botbuilder.ai.luis import LuisRecognizer
 from botbuilder.core import IntentScore, TopIntent, TurnContext
-
 from booking_details import BookingDetails
-
+from datetime import date
 
 class Intent(Enum):
     BOOK_FLIGHT = "book"
@@ -99,23 +98,74 @@ class LuisHelper:
                     result.travel_budget = None
 
 
+                date_start=recognizer_result.entities.get("str_date", [])
+                date_end=recognizer_result.entities.get("end_date", [])
+               # print(type(date_start))
+               # print(date_end)
+     
                 # This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop
                 # the Time part. TIMEX is a format that represents DateTime expressions that include some ambiguity.
                 # e.g. missing a Year.
                 date_entities = recognizer_result.entities.get("datetime", [])
-                #print(len(date_entities))
+                
+                print("--------------------")
+                date_voyage=[]
                 if date_entities:
-                    timex = date_entities[0]["timex"]
-                    timex_ret = date_entities[1]["timex"]
-                    if timex:
-                        datetime = timex[0].split("T")[0]
-                        result.travel_date = datetime
-                    if timex_ret :
-                        datetime_ret = timex_ret[0].split("T")[0]
-                        result.return_date = datetime_ret
+                    for dates in date_entities:
+                        if dates['type']=='date':
+                            data=dates['timex'][0]
+                            #si la date renvoyé ne contient pas l'année
+                            data=data.replace('XXXX', str(date.today().year))
+                        elif dates['type']=='daterange':
+                            data = dates['timex'][0]
+                            data = data.replace('(','')
+                            data = data.replace(')','')
+                            data = data.split(',')
+                            if len(date_entities)>1:
+                                date_voyage.append(data[0].replace('XXXX', str(date.today().year)))
+                                date_voyage.append(data[1].replace('XXXX', str(date.today().year))) 
+                            else:
+                                 data = data[0].replace('XXXX', str(date.today().year))  
+                        date_voyage.append(data)        
+                
+                    if len(date_voyage) > 1:
+                        if date_voyage[0]<date_voyage[1]:
+                            result.travel_date = date_voyage[0]
+                            result.return_date = date_voyage[1]
+                        else:
+                            result.travel_date = date_voyage[1]
+                            result.return_date = date_voyage[0]
+                    else:
+                        result.travel_date=date_voyage[0]
+                              
                 else:
                     result.travel_date = None
-                    result.return_date = None
+                    result.return_date = None    
+                         
+                print("--------------------")
+                print(date_start)
+                print(date_end)
+                print(f"date_allée : {result.travel_date}")
+                print(f"date_retour : {result.return_date}")   
+                      
+                #si l'entities date existe:
+               
+                    # timex == date allèe
+                #timex =     date_entities[0]["timex"]
+                    # si 
+                #if len(date_entities)>=2 :
+                #        timex_ret = date_entities[1]["timex"]
+                #else:
+                #        timex_ret = None   
+                         
+                #if timex:
+                #        datetime = timex[0].split("T")[0]
+                        #print(datetime)
+                #        result.travel_date = datetime
+                #if timex_ret :
+                #        datetime_ret = timex_ret[0].split("T")[0]
+                #        result.return_date = datetime_ret
+                
 
         except Exception as exception:
             print(exception)
